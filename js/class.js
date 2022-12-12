057ef903b60a82c9f9715be65ea297d7a8c3719e
MatiasRicci <matricc89@gmail.com>
2022-12-12 00:20:49 -0300

    modificando3

---
+++ js/class.js
@@ -0,0 +1,44 @@
+class Commit{
+    constructor(name,message,year){
+        this.name = name
+        this.message = message
+        this.year = year
+    }
+}
+
+class UI{
+    addCommit(text){
+       const commitList = document.getElementById('product-list')
+       const elment = document.createElement('div')
+       elment.innerHTML = `
+            <div class="card text-center mb-4">
+                    <div class="card-body">
+                    <strong>Nombre</strong>:${text.name}
+                    <strong>Mensaje</strong>:${text.message}
+                    <strong>Años</strong>:${text.year}
+                    </div>
+            </div>
+       `;
+       commitList.appendChild(elment)
+    }
+    resetForm(){
+        document.getElementById('product-form').reset()
+    }
+   
+}
+
+document.getElementById('product-form')
+.addEventListener('submit', function(e){
+    const name = document.getElementById('name').value
+    const message = document.getElementById('message').value
+    const year = document.getElementById('year').value
+
+    const text = new Commit(name,message,year)
+    
+    
+    const ui = new UI()
+    ui.addCommit(text)
+    ui.resetForm()
+
+    e.preventDefault()
+}) 
